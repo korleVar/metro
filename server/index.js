27a588793c016b03377e5c1e4583be274771396d
korleVar <104516945+korleVar@users.noreply.github.com>
2023-09-15 15:24:52 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,7 +7,6 @@ const { connectToDB } = require("./db.js");
 // вызов функции
 connectToDB();
 // импорт модуля http
-
 const http = require("http");
 // устанавливаем значение для переменной
 const hostname = "127.0.0.1";
