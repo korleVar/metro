90c00880af8312d6e304e192bb25be9ad49df98b
Kororoleva Varvara <isip_v.v.koroleva@mpt.ru>
2023-09-19 15:12:35 +0300

    19/09

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const express = require("express");
 const app = express();
 
 // импорт функции из файла
-const { connectToDB } = require("./db.js");
+const { connectToDB } = require("./db");
 
 // вызов функции
 connectToDB();
